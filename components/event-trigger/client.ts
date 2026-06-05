c01769b63a263b677d58e9a16ee0934626df63f9
employee_01 <employee_01@indffrnt.com>
2026-06-05 21:47:16 +1000

    feat: event trigger base

--- components/event-trigger/client.ts
+++ components/event-trigger/client.ts
@@ -1,38 +1,46 @@
-import { LitElement, html } from "lit";
+import { html, unsafeCSS, nothing } from "lit";
 import type { TemplateResult } from "lit";
-import { customElement } from "lit/decorators.js";
-import { TailwindProvider } from "../../providers/tailwind";
+import { TailwindProvider } from "../../providers/tailwind.js";
+import { customElement } from "lit/decorators/custom-element.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+import { Ref, ref, createRef } from "lit/directives/ref.js";
+import { unsafeHTML } from "lit/directives/unsafe-html.js";
 
-interface EventTriggerProps {
-  details?: Record<string, any>;
+type Props = {
   event: string;
-  html: string;
-  href: string;
-  namespace: string;
-  aria_label: string;
+  href?: string;
   type: string;
-  class: string;
-}
+  aria_label?: string;
+  class?: string;
+  html: string;
+  detail?: Record<string, any>;
+};
 
 @customElement("event-trigger")
-export class EventTrigger extends TailwindProvider<EventTriggerProps> {
-  private _handleClick(): void {
-    const { namespace, event } = this.props;
-    const eventName = `${namespace}:${event}`;
-    const customEvent = new CustomEvent(eventName, {
-      detail: this.props.details,
+class EventTrigger extends TailwindProvider<Props> {
+  private _handleAction(): void {
+    if (!this.props.event) {
+      throw new Error("Please pass event to trigger");
+    }
+
+    const customEvent = new CustomEvent(this.props.event, {
+      detail: this.props.detail,
     });
+
     window.dispatchEvent(customEvent);
   }
 
-  protected render(): TemplateResult {
+  protected render(): TemplateResult | typeof nothing {
+    if (!this.props) return nothing;
+
     return html`<button
+      class="${this.props.class} block"
       type=${this.props.type}
-      class=${this.props.class}
-      aria-label=${this.props.aria_label}
-      @click=${this._handleClick}
+      @click=${this._handleAction}
+      aria-label="${ifDefined(this.props.ariaLabel)}"
+      ${ref(this.buttonRef)}
     >
       ${unsafeHTML(this.props.html)}
-    </button>`;
+    </button> `;
   }
 }
